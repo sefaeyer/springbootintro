10d1edf52ca774f3fb4081d5e3e0983432925e8d
Sefa Eyer <eyersefa@gmail.com>
2024-05-03 00:44:34 +0300

    SpringBoot/ Entities

--- src/main/java/com/tpe/domain/Student.java
+++ src/main/java/com/tpe/domain/Student.java
@@ -10,16 +10,18 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor //student icinde yazilan her variable icin const uretir
+@NoArgsConstructor //parametresiz const
 
 @Entity
 public class Student { //student
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE) //bu degiskeni setlenemez hale getirir
     private Long id;
+
+    //----------------------------------------------------------------
 
     @NotNull(message = "first name can not be null")
     @NotBlank(message = "first name can not be white space")
@@ -27,17 +29,27 @@ public class Student { //student
     @Column(nullable = false, length = 25) // -->  En son asamada kontrol eder
     private String name;
 
+    //----------------------------------------------------------------
+
     @Column(nullable = false, length = 25)
     private String lastName;
 
+    //----------------------------------------------------------------
+
     @Column
     private Integer grade;
+
+    //----------------------------------------------------------------
 
     @Column(nullable = false, length = 60, unique = true)
     @Email(message = "Provide valid email")
     private String email;
 
+    //----------------------------------------------------------------
+
     private String phoneNumber;
+
+    //----------------------------------------------------------------
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy HH:mm:ss", timezone = "Turkey") //database deki veri degismez
     @Setter(AccessLevel.NONE)
